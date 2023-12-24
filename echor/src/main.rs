f95e7b09a58aae8f39959994846a3d0712c25e83
waridh <waridh@ualberta.ca>
2023-12-24 08:37:43 -0700

    completed a basic version of the rust echo program

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -3,9 +3,8 @@ use clap::{
     Arg
 };
 
-fn main() {
-    // This section is just registering the application as a commandline one.
-    let matches = App::new("echor")
+fn cmd_args() -> clap::ArgMatches<'static> {
+    App::new("echor")
         .version("0.1.0")
         .author("Bach")
         .about("Rust commandline suite, echo")
@@ -22,7 +21,16 @@ fn main() {
             .help("Do not print trailing newline")
             .takes_value(false),
             )
-        .get_matches();
+        .get_matches()
+}
 
-    println!("{:?}",matches);
+fn main() {
+    // This section is just registering the application as a commandline one.
+    let matches = cmd_args();
+
+    let output = matches.values_of_lossy("text").unwrap();
+
+    print!("{}{}", output.join(" "),
+        if matches.is_present("omit_newline") {""} else {"\n"})
+
 }
