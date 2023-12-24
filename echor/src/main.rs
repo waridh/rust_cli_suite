7de8f66880edc9711537eba4d8350ad3df49d7f3
waridh <waridh@ualberta.ca>
2023-12-24 07:37:07 -0700

    added command line arguments to the program using clap

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,10 +1,28 @@
-use clap::App;
+use clap::{
+    App,
+    Arg
+};
 
 fn main() {
     // This section is just registering the application as a commandline one.
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Bach")
         .about("Rust commandline suite, echo")
+        .arg(
+            Arg::with_name("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .min_values(1),
+            )
+        .arg(
+            Arg::with_name("omit_newline")
+            .short("n")
+            .help("Do not print trailing newline")
+            .takes_value(false),
+            )
         .get_matches();
+
+    println!("{:?}",matches);
 }
